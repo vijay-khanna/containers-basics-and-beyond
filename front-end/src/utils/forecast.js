18d40e33aa778e88f2ec0a4698392b6c3753a2d8
vijay-khanna <vj97531@gmail.com>
2019-08-13 23:03:33 +0530

    Update forecast.js

--- front-end/src/utils/forecast.js
+++ front-end/src/utils/forecast.js
@@ -1,4 +1,4 @@
-const URLPiArray = 'http://back-end-pi-array-service:90/pi'  
+const URLPiArray = 'http://back-end-pi-array-service:90/pi'
 var urlMotm = `http://MOTMLBURL:91/motm`
 const yargs = require('yargs')
 const chalk = require('chalk')
@@ -6,7 +6,7 @@ var request = require('request');
 var deasync = require("deasync")
 var EventEmitter = require("events").EventEmitter;
 var bodyem = new EventEmitter();
-var sleep = require('thread-sleep');
+//var sleep = require('thread-sleep');
 //var sleep = require('sleep');
 var globalresult = {}
           var piValue = 0
@@ -24,7 +24,7 @@ var globalresult = {}
             .then((res) => {
              // console.log(`statusCode: ${res.statusCode}`)
               //console.log(res.data)
-              
+
             //  console.log('2. server response:' + res.data.unique)
               this.valid = res.data.unique
             //  console.log(res.data.piValue)
@@ -35,7 +35,7 @@ var globalresult = {}
               randomWait = res.data.randomWait
               console.log(piValue, sumArray, randomWait)
               return res.data.unique;
-              
+
               //return res.data;
             })
             .catch((error) => {
@@ -52,13 +52,13 @@ axiosgetmotm.get(urlMotm)
    // console.log(response.data.url);
     console.log(response.data.motm);
     motm = response.data.motm
-    
+
   })
   .catch(error => {
     console.log(error);
   });
-  
-  
+
+
 
 
 
@@ -70,28 +70,28 @@ const forecast = (latitude, longitude, DarkSkyAPISecret, weatherUnits, weatherLa
 //const darkSkyNetURLString = 'https://api.darksky.net/forecast/'+DarkSkyAPISecret+'/'+latitude+','+longitude+'?units='+weatherUnits+'&lang='+weatherLanguage
 const url = 'https://api.darksky.net/forecast/'+DarkSkyAPISecret+'/'+latitude+','+longitude+'?units='+weatherUnits+'&lang='+weatherLanguage
     //const mapBoxForwardEncodingURLString = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token='+MapBoxAccessToken+'&limit=1'
-    // encode will put %20 instead of Space. and manage special characters. 
+    // encode will put %20 instead of Space. and manage special characters.
 
-    request({url, json : true },(error,{ body }) => {  
+    request({url, json : true },(error,{ body }) => {
         if(error){
             callback('Unable to Connect to Weather Service DarkSkyNet API. Possibly Internet/Wifi Issue', undefined)
             console.log(chalk.red.inverse.bold('Unable to Connect to Weather Service DarkSkyNet API.. Possibly Internet connection/Wifi issue'))
-    
+
         }else if (body.error) {
             callback('Unable to find Location in DarkSkyNet API, try another address', undefined)
             //console.log(chalk.red.inverse.bold('\n Unable to find Location in Darksky API'))
             }
-       
+
         else{
            var  percentRainChance = (body.currently.precipProbability*100)
             percentRainChance = percentRainChance.toFixed(2)        //Limiting the Percent to 2 digits
-            
+
          ///-->
-         
+
           var piDigits = body.currently.temperature
           console.log(piDigits)
 
-    /*      
+    /*
           let options = {
           url: URLPiArray,
           method: "POST",
@@ -100,7 +100,7 @@ const url = 'https://api.darksky.net/forecast/'+DarkSkyAPISecret+'/'+latitude+',
           },
           json: {"piDigits": piDigits}
           };
-    
+
           request(options, function(err, res, data) {
               if (!err && res.statusCode == 200) {
                     bodyem.data = data;
@@ -108,41 +108,41 @@ const url = 'https://api.darksky.net/forecast/'+DarkSkyAPISecret+'/'+latitude+',
                     var varpiArrayVal = bodyem.data
                     console.log(varpiArrayVal)
                     bodyem.emit('update');
-                    
+
               }
            }
-          
+
         )
-        
-        */ 
+
+        */
         //...Just about good
 
-    /*    
+    /*
 bodyem.on('update', function () {
-  //  console.log(bodyem.data); 
+  //  console.log(bodyem.data);
 })
 */
-    
+
 console.log(body.currently.summary+' It is Currently '+body.currently.temperature+' Celcius. There is a '+ percentRainChance+' Percent Chance of rain \n'+'Message of the Moment : '+motm+ '\n Pi Value : '+piValue +' sumArray '+sumArray+' randomWait '+randomWait)
 
 callback(undefined, body.currently.summary+' Its Currently '+body.currently.temperature+' Celcius. There is '+ percentRainChance+' % Chance of rain.* \n ' +'Message of the Moment : '+ motm + ' * Sum of Array : '+sumArray+' randomWait : '+randomWait +' \n * Pi Value : '+piValue)
-     
+
         //Check for Internet Connectivity Errors
         //console.log(response)
         //const data = JSON.parse(response.body)            //Removed this as we are not using json=true for direct JSON format output
-        //console.log(response.body.currently.summary)         //Pulling Specific JSON Object Values. 
-        
+        //console.log(response.body.currently.summary)         //Pulling Specific JSON Object Values.
+
         //console.log(percentRainChance)
-        
+
        //// console.log(chalk.yellow('\n Weather is '+response.body.currently.summary+'. \n'))
         //// console.log(chalk.green.inverse('It is Currently '+ response.body.currently.temperature+' degrees Celcius out there in '+response.body.timezone+' . \n'))
         //// console.log('There is a '+ percentRainChance+' percent chance of rain.\n' )
     }
-       
+
     })
-    
+
 }
-       
+
 module.exports = forecast
 
 // Below to be removed
@@ -157,22 +157,22 @@ request({url: darkSkyNetURL, json : true },(error,response) => {     //json = tr
         console.log(chalk.red.inverse.bold('\n Unable to find Location in Darksky API'))
 
     }
-   
+
     else{
-  
+
     //Check for Internet Connectivity Errors
     //console.log(response)
     //const data = JSON.parse(response.body)            //Removed this as we are not using json=true for direct JSON format output
-    //console.log(response.body.currently.summary)         //Pulling Specific JSON Object Values. 
+    //console.log(response.body.currently.summary)         //Pulling Specific JSON Object Values.
     percentRainChance = (response.body.currently.precipProbability*100)
     percentRainChance = percentRainChance.toFixed(2)        //Limiting the Percent to 2 digits
     //console.log(percentRainChance)
-    
+
     console.log(chalk.yellow('\n Weather is '+response.body.currently.summary+'. \n'))
     console.log(chalk.green.inverse('It is Currently '+ response.body.currently.temperature+' degrees Celcius out there in '+response.body.timezone+' . \n'))
     console.log('There is a '+ percentRainChance+' percent chance of rain.\n' )
 }
-   
+
 })
 
 */
